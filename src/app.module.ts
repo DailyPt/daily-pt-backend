f03619bb9c9e36827e6452e70f1fc121fd869243
junni1111 <wkszlf98@naver.com>
2023-05-11 19:53:28 +0900

    fix : package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ import { FirebaseModule } from './firebase/firebase.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV == 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      envFilePath: process.env.NODE_ENV == 'dev' ? '.env.dev' : '.env.prod',
+      // ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
         // PORT
@@ -41,8 +41,8 @@ import { FirebaseModule } from './firebase/firebase.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
-      // logging: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
       autoLoadEntities: true,
     }),
     JwtModule,
